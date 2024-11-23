8a6733047cc1eb25cb366467c3e7a9b41df34da1
taylrfnt <taylr.fnt@gmail.com>
2024-11-22 18:14:26 -0600

    add toggle options

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,27 +1,27 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "catppuccin",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  theme = "catppuccin",
+  theme_toggle = { "catpuccin", "everforest" },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 M.ui = {
-  telescope ={
-    style = "bordered"
+  telescope = {
+    style = "bordered",
   },
 
   statusline = {
     theme = "vscode_colored",
-  }
+  },
 }
 
 return M
