e3dd6246df0beaf4fb7152a2fd04da6efcb915b3
taylrfnt <taylr.fnt@gmail.com>
2024-11-30 13:29:59 -0600

    add raycast

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -11,8 +11,12 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
   let
     configuration = { pkgs, config, ... }: {
-      # allow unfree app installs
-      nixpkgs.config.allowUnfree = true;
+      nixpkgs = {
+        hostPlatform = "aarch64-darwin"; # The platform the configuration will be used on.
+        config = {
+          allowUnfree = true; # allow unfree app installs
+        };
+      };
 
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
@@ -41,6 +45,7 @@
           pkgs.stylua
           pkgs.zsh-autosuggestions
           pkgs.maccy
+          pkgs.raycast
         ];
 
       # user setup
@@ -75,61 +80,76 @@
         onActivation.upgrade = true;
       };
 
-      # Install nerd font
-      fonts.packages = with pkgs; [
-        # more fonts via `nix repl -f nerd-fonts.`
-        nerd-fonts.jetbrains-mono
-      ];
-
-      # using macos aliases instead of symlinks to allow apps to be indexed by spotlight
-      system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = "/Applications";
-        };
-      in
-        pkgs.lib.mkForce ''
-          # Set up applications.
-          echo "setting up /Applications..." >&2
-          rm -rf /Applications/Nix\ Apps
-          mkdir -p /Applications/Nix\ Apps
-          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-          while read -r src; do
-            app_name=$(basename "$src")
-            echo "copying $src" >&2
-            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-          done
-        '';
-
-      system.defaults = {
-        ## TODO: Add login items for maccy ( not yet supported by nix-darwin )
-        # change default system sound to "Breeze"
-        ".GlobalPreferences"."com.apple.sound.beep.sound" = "/System/Library/Sounds/Blow.aiff";
-        dock = {
-          autohide = true;
-          autohide-delay = 0.0; # no delay for auto-hide/show
-          orientation = "left";
-          show-recents = false; # hide recent apps from dock
-          # set dock apps
-          #dock.persistent-apps = [
-          #  "/System/Applications/Finder.app"
-          #  "/System/Applications/Launchpad.app"
-          #  "{pkgs.kitty}/Applications/kitty.app"
-          #];
-        };
-        finder.FXPreferredViewStyle = "clmv"; # column view
-        loginwindow.GuestEnabled = false; # no guest logins
-        NSGlobalDomain.AppleICUForce24HourTime = true; # enable 24hr time
-        menuExtraClock.Show24Hour = true; # show 24 hr clock in menu/login
-        screencapture.type = "png"; # make screenshots png files
+      # Install fonts
+      fonts = {
+        packages = with pkgs; [
+          # more fonts via `nix repl -f nerd-fonts.`
+          nerd-fonts.jetbrains-mono
+        ];
       };
 
-      # automatically catch (most) new prefs without login/restart
-      system.activationScripts.postUserActivation.text = ''
-        # Following line should allow us to avoid a logout/login cycle
-        /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-      '';
+      system = {
+        # Set Git commit hash for darwin-version.
+        configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        stateVersion = 5;
+
+        # darwin config
+        defaults = {
+          #:: TODO :: Add login items for maccy ( not yet supported by nix-darwin )
+          ".GlobalPreferences"."com.apple.sound.beep.sound" = "/System/Library/Sounds/Blow.aiff";
+          dock = {
+            autohide = true;
+            autohide-delay = 0.0; # no delay for auto-hide/show
+            orientation = "left";
+            mineffect = "scale";
+            minimize-to-application = true;
+            show-recents = false; # hide recent apps from dock
+            # set dock apps
+            #dock.persistent-apps = [
+            #  "/System/Applications/Finder.app"
+            #  "/System/Applications/Launchpad.app"
+            #  "{pkgs.kitty}/Applications/kitty.app"
+            #];
+          };
+          finder.FXPreferredViewStyle = "clmv"; # column view
+          loginwindow.GuestEnabled = false; # no guest logins
+          NSGlobalDomain.AppleICUForce24HourTime = true; # enable 24hr time
+          menuExtraClock.Show24Hour = true; # show 24 hr clock in menu/login
+          screencapture.type = "png"; # make screenshots png files
+          ## :: TODO :: set custom browser default ( CustomPrefs? )
+        };
+
+        activationScripts = {
+          # automatically catch (most) new prefs without login/restart
+          postUserActivation.text = ''
+            # Following line should allow us to avoid a logout/login cycle
+            /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+          '';
+          # using MacOs aliases instead of symlinks to allow apps to be indexed by spotlight
+          applications.text = let
+            env = pkgs.buildEnv {
+              name = "system-applications";
+              paths = config.environment.systemPackages;
+              pathsToLink = "/Applications";
+            };
+          in
+            pkgs.lib.mkForce ''
+              # Set up applications.
+              echo "setting up /Applications..." >&2
+              rm -rf /Applications/Nix\ Apps
+              mkdir -p /Applications/Nix\ Apps
+              find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+              while read -r src; do
+                app_name=$(basename "$src")
+                echo "copying $src" >&2
+                ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+              done
+            '';
+        };
+      };
 
       # Enable alternative shell support in nix-darwin.
       programs.zsh.enable = true; # default shell on catalina
@@ -141,15 +161,6 @@
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
     };
   in
   {
@@ -161,11 +172,9 @@
         nix-homebrew.darwinModules.nix-homebrew
         {
           nix-homebrew = {
-            enable = true;
-            # Apple Silicon Only
-            enableRosetta = true;
-            # User owning the Homebrew prefix
-            user = "taylorfont";
+            enable = true; # enable homebrew via nix
+            enableRosetta = true; # Apple Silicon Only
+            user = "taylorfont"; # User owning the Homebrew prefix
           };
         }
       ];
