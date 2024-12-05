caf92ef91233f697a6db01412dd70516b943fc79
Taylor Font <taylr.fnt@gmail.com>
2024-12-04 18:11:24 -0600

    refactor tmux

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -51,6 +51,61 @@
           pkgs.kubectl
         ];
 
+      # tmux configurations
+      programs.tmux = {
+        enable = true;
+        enableMouse = true;
+        enableSensible = true;
+        enableVim = true;
+        extraConfig = ''
+          # Custom Prefix key (Ctrl-Spc)
+          unbind C-b
+          set -g prefix C-Space
+          bind C-Space send-prefix
+
+          # Start windows and panes at 1, not 0
+          set -g base-index 1
+          set -g pane-base-index 1
+          set-window-option -g pane-base-index 1
+          set-option -g renumber-windows on
+
+          # Use Alt-arrow keys without prefix key to switch panes
+          bind -n M-Left select-pane -L
+          bind -n M-Right select-pane -R
+          bind -n M-Up select-pane -U
+          bind -n M-Down select-pane -D
+
+          # Shift arrow to switch windows
+          bind -n S-Left  previous-window
+          bind -n S-Right next-window
+
+          # Shift Alt vim keys to switch windows
+          bind -n M-H previous-window
+          bind -n M-L next-window
+
+          #Plugins
+          set -g @plugin 'tmux-plugins/tpm'
+          set -g @plugin 'tmux-plugins/tmux-sensible'
+          set -g @plugin 'tmux-plugins/tmux-cpu'
+          set -g @plugin 'christoomey/vim-tmux-navigator'
+          set -g @plugin 'tmux-plugins/tmux-yank'
+
+          run '~/.tmux/plugins/tpm/tpm'
+          run '~/.config/tmux/plugins/catppuccin/tmux/catppuccin.tmux'
+
+          # Catpuccin customizations
+          set -g @catppuccin_flavour 'mocha'
+          set -g @catppuccin_window_status_style "rounded"
+          set -g status-right-length 100
+          set -g status-left-length 100
+          set -g status-left ""
+          set -g status-right "#{E:@catppuccin_status_application}"
+          set -ag status-right "#{E:@catppuccin_status_session}"
+          set -ag status-right "#{E:@catppuccin_status_uptime}"
+          run ~/.config/tmux/plugins/tmux-plugins/tmux-cpu/cpu.tmux
+        '';
+      };
+
       # user setup
       users.users.taylorfont = {
         name = "taylorfont";
