e1a802cd84b941f2f6b92b56a093e85419c30889
Taylor Font <taylr.fnt@gmail.com>
2025-01-07 15:03:44 -0600

    overhaul of base, new ghostty opts

--- nix/darwin/amaterasu/base/configuration.nix
+++ nix/darwin/amaterasu/base/configuration.nix
@@ -11,8 +11,16 @@
   # Enable alternative shell support in nix-darwin.
   # programs.fish.enable = true;
 
-  # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
+  nix = {
+    # Allow building and running binaries for x86_64 and aarch64 (enabled by Rosetta 2)
+    extraOptions = ''
+      extra-platforms = x86_64-darwin aarch64-darwin
+    '';
+    ## Linux builder provided by NixOS VM that works on Apple Silicon & Intel Macs
+    linux-builder.enable = true;
+    ## Necessary for using flakes on this system.
+    settings.experimental-features = "nix-command flakes";
+  };
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
@@ -20,6 +28,10 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = [
+    pkgs.zsh
+    pkgs.oh-my-posh
+    pkgs.zsh-vi-mode
+    pkgs.zsh-autosuggestions
     pkgs.neovim
     pkgs.tmux
     pkgs.git
@@ -27,11 +39,11 @@
     pkgs.gh
     pkgs.pass
     pkgs.gnupg
-    pkgs.oh-my-posh
-    pkgs.zsh-vi-mode
-    pkgs.sketchybar
-    pkgs.pyenv
-    pkgs.mkalias
+    pkgs.raycast
+    pkgs.maccy
+    pkgs.bruno
+    pkgs.mkalias # nix needs to make darwin aliases rather than symlinks for apps
+    pkgs.openssh
     pkgs.yt-dlp
     pkgs.lua-language-server
     pkgs.gopls
@@ -46,21 +58,13 @@
     pkgs.bash-language-server
     pkgs.jq
     pkgs.stylua
-    pkgs.zsh-autosuggestions
-    pkgs.raycast
-    pkgs.maccy
     pkgs.kubectl
-    pkgs.bruno
-    pkgs.zsh
   ];
 
   # Install fonts
   fonts = {
     packages = [
-      pkgs.sketchybar-app-font
-      # more fonts via `nix repl -f nerd-fonts.`
       pkgs.nerd-fonts.jetbrains-mono
-      pkgs.nerd-fonts.noto
       pkgs.nerd-fonts.commit-mono
     ];
   };
@@ -82,17 +86,18 @@
       "oversight"
       "keycastr"
     ];
+
     brews = [
-      "openjdk@17"
-      "openjdk@21"
-      "mas"
+      "mas" # needs xcode-select (TODO: find automated install)
       "coreutils"
       "helm"
       "helmfile"
     ];
+
     masApps = {
       "Yoink" = 457622435;
     };
+
     onActivation.cleanup = "zap";
     onActivation.autoUpdate = true;
     onActivation.upgrade = true;
@@ -107,46 +112,74 @@
 
   environment.variables = {
     # see https://github.com/ghostty-org/ghostty/discussions/2832
-    XDG_DATA_DIRS = ["/Applications/Ghostty.app/Contents/Resources/ghostty/shell-integration"];
+    # XDG_DATA_DIRS = ["/Applications/Ghostty.app/Contents/Resources/ghostty/shell-integration"];
     EDITOR = "nvim";
   };
 
   # login items
   launchd.user.agents = {
+
     raycast = {
       serviceConfig.ProgramArguments =
         [ "${pkgs.raycast}/Applications/Raycast.app/Contents/MacOS/Raycast" ];
       serviceConfig.KeepAlive = true;
     };
+
     maccy = {
       serviceConfig.ProgramArguments =
         [ "${pkgs.maccy}/Applications/Maccy.app/Contents/MacOS/Maccy" ];
       serviceConfig.RunAtLoad = true;
     };
+
     oversight = {
       serviceConfig.ProgramArguments =
         [ "/Applications/OverSight.app/Contents/MacOS/OverSight" ];
       serviceConfig.RunAtLoad = true;
     };
+
     lulu = {
       serviceConfig.ProgramArguments =
         [ "/Applications/LuLu.app/Contents/MacOS/LuLu" ];
       serviceConfig.RunAtLoad = true;
     };
+
     yoink = {
       serviceConfig.ProgramArguments =
         [ "/Applications/Yoink.app/Contents/MacOS/Yoink" ];
       serviceConfig.RunAtLoad = true;
     };
+
   };
 
   system = {
-    # Set Git commit hash for darwin-version.
-    #configurationRevision = self.rev or self.dirtyRev or null;
 
-    # Used for backwards compatibility, please read the changelog before changing.
-    # $ darwin-rebuild changelog
-    stateVersion = 5;
+    activationScripts = {
+      # using MacOs aliases instead of symlinks to allow apps to be indexed by spotlight
+      applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read -r src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
+      # automatically catch (most) new prefs without login/restart
+      postUserActivation.text = ''
+        # Following line should allow us to avoid a logout/login cycle
+        /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+      '';
+    };
 
     #MacOS system configuration
     defaults = {
@@ -182,43 +215,46 @@
           "/Users/taylorfont/Downloads"
         ];
       };
-      finder.FXPreferredViewStyle = "clmv"; # column view
-      loginwindow.GuestEnabled = false; # no guest logins
-      NSGlobalDomain.AppleICUForce24HourTime = true; # enable 24hr time
-      menuExtraClock.Show24Hour = true; # show 24 hr clock in menu/login
+
+      finder = {
+        AppleShowAllExtensions = true;
+        ShowPathbar = true;
+        FXEnableExtensionChangeWarning = false;
+        FXPreferredViewStyle = "clmv";
+        _FXSortFoldersFirst = true;
+      };
+
+      loginwindow = {
+        GuestEnabled = false;
+        LoginwindowText = "amaterasu";
+      };
+
+      menuExtraClock = {
+        Show24Hour = true;
+        ShowSeconds = true;
+      };
+
+      NSGlobalDomain = {
+        AppleICUForce24HourTime = true;
+        AppleInterfaceStyle = "Dark";
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticInlinePredictionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSDocumentSaveNewDocumentsToCloud = false;
+        NSWindowShouldDragOnGesture = true;
+      };
+
       screencapture = {
-        type = "png"; # make screenshots png files
+        type = "png";
         target = "clipboard";
       };
     };
+    # Set Git commit hash for darwin-version.
+    # configurationRevision = self.rev or self.dirtyRev or null;
 
-    activationScripts = {
-      # automatically catch (most) new prefs without login/restart
-      postUserActivation.text = ''
-        # Following line should allow us to avoid a logout/login cycle
-        /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-      '';
-      # using MacOs aliases instead of symlinks to allow apps to be indexed by spotlight
-      applications.text = let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = "/Applications";
-        };
-      in
-        pkgs.lib.mkForce ''
-          # Set up applications.
-          echo "setting up /Applications..." >&2
-          rm -rf /Applications/Nix\ Apps
-          mkdir -p /Applications/Nix\ Apps
-          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-          while read -r src; do
-            app_name=$(basename "$src")
-            echo "copying $src" >&2
-            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-          done
-        '';
-    };
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    stateVersion = 5;
   };
 
 }
